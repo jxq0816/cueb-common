c0794fe9fa59a1d91b6a24ecc3491752fe45b1cb
jiangxingqi <jxq0816@126.com>
2017-12-10 18:55:27 +0800

    数据读取公共接口

---
+++ src/main/java/com/week7i/share/dao/CommonDao.java
@@ -0,0 +1,12 @@
+package com.week7i.share.dao;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by Administrator on 2017/2/23.
+ */
+public interface CommonDao {
+    List<HashMap> queryTbName(Map param);
+}
