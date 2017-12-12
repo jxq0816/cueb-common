e5eecb565da9fc0f40c4516917bf427b604a31db
jiangxingqi <jxq0816@126.com>
2017-12-12 22:33:22 +0800

    王钊数据测试 数据为空过滤

--- src/test/java/CommonTest.java
+++ src/test/java/CommonTest.java
@@ -27,7 +27,7 @@ public class CommonTest {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"applicationContext.xml", "dispatcherServlet.xml", "spring-context-shiro.xml"});
         SpringContextHolder holder = context.getBean(SpringContextHolder.class);
         CommonService service = holder.getBean(CommonService.class);
-        String path = "doc/ETF01.xlsx";
+        String path = "doc/qihuo1.xlsx";
         List<Map> result = ReadExcel.read(path);
         FileWriter fileWriter = null;
         int rowNum=result.size();
@@ -38,65 +38,67 @@ public class CommonTest {
                 String code=(String) m.get("code");
                 String name=(String) m.get("name");
                 System.out.println(code+" start");
-                List<HashMap> rs=service.etf(code);
-                String content="";
-                content+="code";
-                content+=" ";
-                content+="name";
-                content+=" ";
-                content+="date_time";
-                content+=" ";
-                content+="open";
-                content+=" ";
-                content+="high";
-                content+=" ";
-                content+="low";
-                content+=" ";
-                content+="close";
-                content+=" ";
-                content+="volume";
-                content+=" ";
-                content+="amount";
-                content+=" ";
-                content+="position";
-                content+="\r\n";
-                String pre_close="0";
-                for(int j=0;j<rs.size();j++){
-                    System.out.println(j);
-                    HashMap map=rs.get(i);
-                    content+=code;
+                List<HashMap> rs=service.wangzhao(code);
+                if(rs.size()!=0){
+                    String content="";
+                    content+="code";
                     content+=" ";
-                    content+=name;
+                    content+="name";
                     content+=" ";
-                    content+=map.get("date_time");
+                    content+="date_time";
                     content+=" ";
-                    content+=map.get("open");
+                    content+="open";
                     content+=" ";
-                    content+=map.get("high");
+                    content+="high";
                     content+=" ";
-                    content+=map.get("low");
+                    content+="low";
                     content+=" ";
-
-                    String close=(String) map.get("close");
-                    content+=close;
+                    content+="close";
                     content+=" ";
-                    content+=pre_close;//前收盘
-
-                    pre_close=close;
-
-                    content+=map.get("volume");
+                    content+="volume";
                     content+=" ";
-                    content+=map.get("amount");
+                    content+="amount";
                     content+=" ";
-                    content+=map.get("position");
+                    content+="position";
                     content+="\r\n";
+                    Double pre_close=0.0;
+                    for(int j=0;j<rs.size();j++){
+                        System.out.println(j);
+                        HashMap map=rs.get(i);
+                        content+=code;
+                        content+=" ";
+                        content+=name;
+                        content+=" ";
+                        content+=map.get("date_time");
+                        content+=" ";
+                        content+=map.get("open");
+                        content+=" ";
+                        content+=map.get("high");
+                        content+=" ";
+                        content+=map.get("low");
+                        content+=" ";
+
+                        Double close=(Double) map.get("close");
+                        content+=close;
+                        content+=" ";
+                        content+=pre_close;//前收盘
+
+                        pre_close=close;
+
+                        content+=map.get("volume");
+                        content+=" ";
+                        content+=map.get("amount");
+                        content+=" ";
+                        content+=map.get("position");
+                        content+="\r\n";
+                    }
+                    String file = "doc/"+code+".txt";
+                    fileWriter = new FileWriter(file);
+                    fileWriter.write(content);
+                    fileWriter.flush();
+                    fileWriter.close();
+                    System.out.println(code+" end");
                 }
-                String file = "doc/"+code+".txt";
-                fileWriter = new FileWriter(file);
-                fileWriter.write(content);
-                fileWriter.flush();
-                fileWriter.close();
-                System.out.println(code+" end");
             }
         }
     }
